c6db6cc3764b9d8f41337533634b5958839463a7
bandhandut19 <bondon21081@gmail.com>
2024-11-22 18:08:12 +0600

    initial setup done with mongodb

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,31 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow only this origin
+  // origin: 'https://bond-bike-rental-frontend.vercel.app', // Allow only this origin
+  credentials: true, // Allow credentials (cookies, authorization headers)
+}
+
 //parsers
 app.use(express.json())
-app.use(cors())
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173')
+  // res.header(
+  //   'Access-Control-Allow-Origin',
+  //   'https://bond-bike-rental-frontend.vercel.app',
+  // );
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  )
+  next()
 })
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Bond Portfolio')
+})
 export default app
