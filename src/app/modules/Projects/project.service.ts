df6b4f20bf2bf82ffce4a003831829d8b4554cdc
bandhandut19 <bondon21081@gmail.com>
2024-12-10 08:32:08 +0600

    created services for projects for crud op

---
+++ src/app/modules/Projects/project.service.ts
@@ -0,0 +1,35 @@
+import { TProject } from './project.instance'
+import { Project } from './project.model'
+
+const addProjectsIntoDB = async (payload: TProject) => {
+  const result = await Project.create(payload)
+  return result
+}
+const getProjectsFromDB = async () => {
+  const result = await Project.find()
+  return result
+}
+const deleteProjectFromDB = async (id: string) => {
+  const result = await Project.findByIdAndDelete(id)
+  return result
+}
+
+const updateProjectsIntoDB = async (payload: TProject, id: string) => {
+  const result = await Project.findByIdAndUpdate(
+    id,
+    {
+      $set: payload,
+    },
+    {
+      new: true,
+    },
+  )
+  return result
+}
+
+export const ProjectServices = {
+  addProjectsIntoDB,
+  getProjectsFromDB,
+  updateProjectsIntoDB,
+  deleteProjectFromDB,
+}
